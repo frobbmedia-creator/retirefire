bbf349ee72ba0e5f53ebd08d316b8e15ef783f54
frobbmedia-creator <frobbmedia@gmail.com>
2026-09-10 15:09:14 -0400

    Restore evergreen decision pages part A + combiner

---
+++ src/content/decision-evergreen.ts
@@ -0,0 +1,4 @@
+import { evergreenPagesA } from "@/content/decision-evergreen-a";
+import { evergreenPagesB } from "@/content/decision-evergreen-b";
+
+export const evergreenPages = [...evergreenPagesA, ...evergreenPagesB];
